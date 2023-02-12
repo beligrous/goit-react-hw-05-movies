99b98719a1e4ba5b40b6850ca538b888c1748aa7
beligrous <beligrous@gmail.com>
2023-02-12 11:24:16 +0200

    add reviews

--- src/components/Modules/MovieDetails/Reviews/Reviews.jsx
+++ src/components/Modules/MovieDetails/Reviews/Reviews.jsx
@@ -1,5 +1,35 @@
+import { getReviews } from 'service/api/getFilms';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+
+
 function Reviews() {
-  return <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi eius alias at quam illum dicta accusamus, repellat iste harum, dolores reprehenderit nobis veritatis tempora laudantium corrupti. Accusantium obcaecati sed nisi?</p>;
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const[error, setError] = useState(null);
+  const { id }  = useParams();
+
+  useEffect(() => {
+    const fetchReviews = async id => {
+      setLoading(true);
+      try {
+        const data = await getReviews(id);
+          setItems(data.results);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+      fetchReviews(id);
+  },[id]);
+
+  const elements = items.map(({id, content, author})=><li key={id}><p>Author: {author}</p><p>{content}</p></li>)
+
+  return (<>{loading && <p>please wait, loading...</p>}
+  <ul>{elements}</ul>
+  {error && <p>{error}</p>}</>);
 }
 
 export default Reviews;
